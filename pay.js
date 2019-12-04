435d741e2d8be31e7bb08e430b328fb473834373
saurabh-nagaich <58461721+saurabh-nagaich@users.noreply.github.com>
2019-12-04 13:46:50 +0530

    Create pay.js

---
+++ pay.js
@@ -0,0 +1,17 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }  
+  function sigin() {
+    document.getElementById("check").style.display="block";
+    document.getElementById("abhi").style.display="none"
+  }
+  function sigout() {
+    document.getElementById("check").style.display="none";
+    document.getElementById("abhi").style.display="block"
+  }
+
+  
